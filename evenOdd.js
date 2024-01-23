1d428854552bb6119d5b7629dd47e35fc55665e7
ayashadevikar <ayashadevikar8@gmail.com>
2024-01-23 09:47:23 +0530

    first commit

---
+++ evenOdd.js
@@ -0,0 +1,71 @@
+// even odd numbers
+
+// const number = prompt("Enter a no. : ");
+
+// if(number%2 == 0){
+//     window.alert("no. is even");
+// }
+// else{
+//     window.alert("no. is odd");
+// }
+
+// using ternatory operator 
+
+
+// const result = (number %2 == 0) ? "even" : "odd";
+// window.alert(`the no. is ${result}`);
+
+
+// for loop
+
+// for(let i = 0; i < 10; i++){
+//     if(i % 2 == 0){
+//         console.log(i, "is even");
+//     } else {
+//         console.log(i, "is odd");
+//     }
+// }
+
+
+// while loop
+
+
+// let i = 1;
+
+// while (i <= 10) {
+//     if (i %2 == 0) {
+//         console.log(i + "is even");
+//     } else {
+//         console.log(i + "is odd");
+//     }
+//     i++;
+// }
+
+
+// do while
+
+// let z = 1;
+
+// do {
+//     if (z % 2 == 0) {
+//         console.log(z + "is even");
+//     } else {
+//         console.log(z + "is odd");
+//      }
+//      z++;
+// } while (z <= 10)
+
+// let x = 1;
+
+// do{
+//      if (x % 2 == 0) {
+//         console.log(x + "is even");
+//      } else {
+//         console.log(x + "is odd");
+//      }
+//      x++;
+// } while (x <= 10)
+
+
+
+
---
+++ factorial.js
@@ -0,0 +1,11 @@
+const number = 5;
+
+let factorialResult =  1;
+
+for (let i = 1; i <= number; i++) {
+    factorialResult *= i;
+}
+
+console.log(`the factorial of ${number} is: ${factorialResult}`);
+
+
